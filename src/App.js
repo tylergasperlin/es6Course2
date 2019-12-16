afec6820e1247e7ae6a6b87582fcb59910adf4b8
Tyler <gasperlint@gmail.com>
2019-12-15 16:45:11 -0800

    find lect 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import active from './es-six-lectures/filter/filter3'
+import active from './es-six-lectures/find/find'
 
 class App extends React.Component {
   componentDidMount(){
---
+++ src/es-six-lectures/find/find.js
@@ -0,0 +1,37 @@
+
+
+//find
+var users = [
+    {name: 'Jill'},
+    {name: 'Alex'},
+    {name: 'Bill'},
+
+ ]
+ 
+  const active = () =>{
+    console.log('old way!')
+        var user;
+        for (var i = 0; i <users.length; i++)
+        if(users[i].name ==='Alex'){
+            user = users[i]
+            break
+        }
+        console.log(user)
+
+    console.log('new way')
+        var u = users.find(function(user){
+            //triple equal returns true or false 
+            return user.name === 'Alex'
+        })
+        
+
+    console.log(u)
+    console.log('arrow')
+        var uA = users.find(user =>{
+            return user.name === 'Alex'
+        })
+    console.log(uA)
+ 
+  }
+ 
+  export default active;
